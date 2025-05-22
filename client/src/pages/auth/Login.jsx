663ca3cfe70b07e71e454a12849ad16707312d8c
Cecilia <ceci_2504@hotmail.com>
2025-05-22 21:47:09 +0200

    Adjust Login.jsx to newer version

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Link } from 'react-router-dom';
 import fetchData from "../../utils/fetchData";
-import "./Auth.css"
-
+import { AuthContext } from "../../components/authContext/AuthContext";
+import "./Auth.css" 
+import Register from "./Register";
 // LOGIN 
 function Login() {
+    const { setToken, setNickname, setEmail } = useContext(AuthContext);
     const [error, setError] = useState(null);
+    const [displaySignup, setDisplaySignup] = useState(false);
     const [userData, setUserData] = useState({
         email: "",
         password: ""
     })
 
+    const toggleSignup = () =>{
+        setDisplaySignup(!displaySignup);
+    }
     const handleUserEmail = (e) => {
         setUserData({ ...userData, email: e.target.value });
     };
@@ -22,57 +29,53 @@ function Login() {
         e.preventDefault();
 
         try {
-
             if (!userData.email || userData.email.trim() === "" || !userData.password) {
                 setError("Debes completar ambos campos");
                 return;
             }
-
-
-            const loginResponse = await fetchData("/login", "POST", userData); 
-
+            const loginResponse = await fetchData("/login", "POST", userData);
             if (loginResponse.error) {
                 setError(loginResponse.error);
                 return;
             }
-
             if (loginResponse.token) {
             localStorage.setItem('authToken', loginResponse.token);
+            setToken(loginResponse.token);
+            setNickname(loginResponse.userData.nickname);
+            setEmail(loginResponse.userData.email);
             setUserData({ email: "", password: "" }); 
-            alert("Login exitoso!");
+            /* alert("¡Registro exitoso!"); */
             }
-
             setError(null);
-            
         } catch (error) {
-            
             setError("Hubo un problema con la solicitud. Intenta de nuevo.");
-            
-        
         }
     };
 
-
     return (
-        <section className="auth-wrapper">
-
-            <section className="auth__header">
-                <h1>Inicia sesión</h1>
-                <p className="error">{error}</p>
+        !displaySignup ? <>
+            <section className="auth__container">
+                <div className="auth__image">
+                    <img src="src/assets/images/Register.png" alt="img" />
+                </div>
+                <section className="auth-wrapper">
+                    <section className="auth__header">
+                        <h1>Inicia sesión</h1>
+                        <p className="error">{error}</p>
+                    </section>
+                    <form className="auth__form" onSubmit={handleSubmit}>
+                        <label htmlFor="email">Correo electrónico</label>
+                        <input type="email" name="email" id="email" value={userData.email} onChange={handleUserEmail} />
+                        <label htmlFor="password">Contraseña</label>
+                        <input type="password" name="password" id="password" value={userData.password} onChange={handleUserPassword} />
+                        <button className="auth__button">Acceder</button>
+                    </form>
+                    <section className="auth__redirect">
+                        <p onClick={toggleSignup}>¿No tienes una cuenta? </p>
+                    </section>
+                </section>
             </section>
-
-            <form className="auth__form" onSubmit={handleSubmit}>
-                <label htmlFor="email">Correo electrónico</label>
-                <input type="email" name="email" id="email" value={userData.email} onChange={handleUserEmail} />
-                <label htmlFor="password">Contraseña</label>
-                <input type="password" name="password" id="password" value={userData.password} onChange={handleUserPassword} />
-                <button className="auth__button">Acceder</button>
-            </form>
-           {/*  <section className="auth__redirect">
-                <p>¿No tienes una cuenta? </p>
-                <Link to="/registro">Regístrate</Link>
-            </section> */}
-        </section>
+        </> : <Register toggleSignup={toggleSignup}/>
     )
 }
 
