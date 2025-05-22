9af507f5c88f8e38270a24bf2c86d3eec8fd204b
Carlos <carlosborjawebdev@gmail.com>
2025-05-22 16:57:56 +0200

    Add JoinGameButton component

---
+++ client/src/components/joinGameButton/JoinGameButon.jsx
@@ -0,0 +1,23 @@
+import fetchData from "../../utils/fetchData";
+import { useState } from "react";
+
+const JoinGameButton = ({nickname}) => {
+    const [gameId, setGameId] = useState(null);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const gameId = e.target.gameIdField.value;
+        const response = await fetchData(`/joinPlayer/${gameId}`, "POST");
+        console.log(response);
+    }
+    return(
+        <p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="gameIdField">Game Id: </label><input type="text" name="gameIdField" id="gameIdField" />
+                <label htmlFor="userNickname">Nickname: </label><input type="text" name="userNickname" id="userNickname" value={nickname} />
+                <button type="submit">Join Game!</button>
+            </form>
+        </p>
+    )
+}
+
+export default JoinGameButton;
