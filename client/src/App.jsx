9ac4d87f025c5f4f7c42f659e04ba85e39843669
Carlos <carlosborjawebdev@gmail.com>
2025-05-22 17:02:02 +0200

    fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider, AuthContext } from "./components/authContext/AuthContext";
 import { useContext } from "react";
-
+import JoinGameButton from "./components/joinGameButton/JoinGameButton";
 import Login from "./pages/auth/Login";
 // asdasd2@asd.com
 
@@ -13,8 +13,14 @@ function AppContent() {
       <nav>
         <h1>Erraton</h1>
           {nickname ? <p>Logeado como {nickname} !</p> : <Login />}
+        <JoinGameButton/>
         <p>El token es: {token}</p>
       </nav>
+      <main>
+        <Routes>
+          <Route path="/create" element={<h1>Home</h1>} />
+        </Routes>
+      </main>
     </>
   );
 }
