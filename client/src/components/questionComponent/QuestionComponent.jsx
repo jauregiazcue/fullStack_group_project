41644bc4e62464a56bb569d7ca5331df5ca995b5
Kai <jauregiazcue@gmail.com>
2025-05-27 15:24:23 +0200

    continue working on questioncomponent

--- client/src/components/questionComponent/QuestionComponent.jsx
+++ client/src/components/questionComponent/QuestionComponent.jsx
@@ -1,31 +1,37 @@
 import { nanoid } from 'nanoid'
 import { useState } from 'react'
-const QuestionComponent = ({question}) => {
+const QuestionComponent = ({ question }) => {
     const [resultado, setResultado] = useState(false);
-    const checkAnswer = (answerIdentifier, keyAnswers) =>{
-        return (keyAnswers.find((element)=>{
+
+    const checkAnswer = (answerIdentifier, keyAnswers) => {
+        return (keyAnswers.find((element) => {
             return element[0] == answerIdentifier;
         }))[1].toString();
     }
-    
+
     const fragments = question.match(/([#@][^#@]+[#@])/g);
     const keyAnswers = []
-    const idFragments = fragments.map((fragment)=>{
+    const idFragments = fragments.map((fragment) => {
         const identifier = nanoid();
-        if(fragment[0] === "@"){
+        if (fragment[0] === "@") {
             keyAnswers.push([identifier, true])
-        }else{
+        } else {
             keyAnswers.push([identifier, false])
         }
-        return [identifier, fragment.substring(1,fragment.length-1)];
+        return [identifier, fragment.substring(1, fragment.length - 1)];
     })
     console.log("Los ids son", keyAnswers)
     console.log("Los fragments son", idFragments);
-    return(
+
+    const handleClick = (text) => {
+        setResultado(checkAnswer(text[0], keyAnswers))
+    }
+    return (
         <div>
-            <h3>La respuesta clickada es: {resultado}</h3>
-            {idFragments.map((text)=>{
-                return(<p onClick={()=>{setResultado(checkAnswer(text[0], keyAnswers))}} key={text[0]}>{text[1]}</p>)
+            {idFragments.map((text) => {
+                return (<p onClick={handleClick(text)} key={text[0]}>
+                    {text[1]}
+                </p>)
             })}
         </div>
     )
